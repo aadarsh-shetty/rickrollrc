0fc96cf6d384f04e1bbac5b7a919e6b9b047c3c6
Aadarsh Shetty <ashetty@netflix.com>
2024-07-14 23:37:05 +0530

    This script is adapted for zsh on MacOS 14.5. The key changes are:
    -Changed the shebang to #!/bin/zsh.
    -Updated NEVER_GONNA to use zsh instead of bash.
    -Changed the path for injecting into the zsh configuration file to ~/.zshrc.
    -Removed support for Linux and Windows specific audio playback tools.
    -Updated the python invocation to python3 for better compatibility with MacOS 14.5.
    -Changed function definitions to be compatible with zsh by using function keyword.
    -Updated has? to has to avoid using characters not typically allowed in function names.
    -Use a temporary file to store the video data before passing it to the Python script.
    -The Python script reads from the temporary file instead of using a here-document.

--- roll.sh
+++ roll.sh
@@ -1,34 +1,38 @@
-#!/bin/bash
+#!/bin/zsh
 # Rick Astley in your Terminal.
 # By Serene and Justine Tunney <3
 version='1.2'
 rick='https://keroserene.net/lol'
 video="$rick/astley80.full.bz2"
-# TODO: I'll let someone with mac or windows machine send a pull request
-# to get gsm going again :)
-audio_gsm="$rick/roll.gsm"
 audio_raw="$rick/roll.s16"
 audpid=0
-NEVER_GONNA='curl -s -L http://bit.ly/10hA8iC | bash'
-MAKE_YOU_CRY="$HOME/.bashrc"
+NEVER_GONNA='curl -s -L http://bit.ly/10hA8iC | zsh'
+MAKE_YOU_CRY="$HOME/.zshrc"
 red='\x1b[38;5;9m'
 yell='\x1b[38;5;216m'
 green='\x1b[38;5;10m'
 purp='\x1b[38;5;171m'
 echo -en '\x1b[s'  # Save cursor.
 
-has?() { hash $1 2>/dev/null; }
-cleanup() { (( audpid > 1 )) && kill $audpid 2>/dev/null; }
-quit() { echo -e "\x1b[2J \x1b[0H ${purp}<3 \x1b[?25h \x1b[u \x1b[m"; }
+function has {
+  command -v $1 >/dev/null 2>&1
+}
+function cleanup {
+  (( audpid > 1 )) && kill $audpid 2>/dev/null
+}
+function quit {
+  echo -e "\x1b[2J \x1b[0H ${purp}<3 \x1b[?25h \x1b[u \x1b[m"
+}
 
-usage () {
+function usage {
   echo -en "${green}Rick Astley performs ♪ Never Gonna Give You Up ♪ on STDOUT."
   echo -e "  ${purp}[v$version]"
   echo -e "${yell}Usage: ./astley.sh [OPTIONS...]"
   echo -e "${purp}OPTIONS : ${yell}"
   echo -e " help   - Show this message."
-  echo -e " inject - Append to ${purp}${USER}${yell}'s bashrc. (Recommended :D)"
+  echo -e " inject - Append to ${purp}${USER}${yell}'s zshrc. (Recommended :D)"
 }
+
 for arg in "$@"; do
   if [[ "$arg" == "help"* || "$arg" == "-h"* || "$arg" == "--h"* ]]; then
     usage && exit
@@ -44,55 +48,58 @@ for arg in "$@"; do
     usage && exit
   fi
 done
+
 trap "cleanup" INT
 trap "quit" EXIT
 
 # Bean streamin' - agnostic to curl or wget availability.
-obtainium() {
-  if has? curl; then curl -s $1
-  elif has? wget; then wget -q -O - $1
+function obtainium {
+  if has curl; then curl -s $1
+  elif has wget; then wget -q -O - $1
   else echo "Cannot has internets. :(" && exit
   fi
 }
+
 echo -en "\x1b[?25l \x1b[2J \x1b[H"  # Hide cursor, clear screen.
 
-#echo -e "${yell}Fetching audio..."
-if has? afplay; then
+# Fetching audio...
+if has afplay; then
   # On Mac OS, if |afplay| available, pre-fetch compressed audio.
   [ -f /tmp/roll.s16 ] || obtainium $audio_raw >/tmp/roll.s16
   afplay /tmp/roll.s16 &
-elif has? aplay; then
-  # On Linux, if |aplay| available, stream raw sound.
-  obtainium $audio_raw | aplay -Dplug:default -q -f S16_LE -r 8000 &
-elif has? play; then
-  # On Cygwin, if |play| is available (via sox), pre-fetch compressed audio.
-  obtainium $audio_gsm >/tmp/roll.gsm.wav
-  play -q /tmp/roll.gsm.wav &
 fi
 audpid=$!
 
-#echo -e "${yell}Fetching video..."
-# Sync FPS to reality as best as possible. Mac's freebsd version of date cannot
-# has nanoseconds so inject python. :/
-python <(cat <<EOF
+# Fetching video...
+tmpfile=$(mktemp)
+obtainium $video | bunzip2 -q > $tmpfile
+
+python3 <<EOF
 import sys
 import time
-fps = 25; time_per_frame = 1.0 / fps
-buf = ''; frame = 0; next_frame = 0
+
+fps = 25
+time_per_frame = 1.0 / fps
+buf = ''
+frame = 0
+next_frame = 0
 begin = time.time()
 try:
-  for i, line in enumerate(sys.stdin):
-    if i % 32 == 0:
-      frame += 1
-      sys.stdout.write(buf); buf = ''
-      elapsed = time.time() - begin
-      repose = (frame * time_per_frame) - elapsed
-      if repose > 0.0:
-        time.sleep(repose)
-      next_frame = elapsed / time_per_frame
-    if frame >= next_frame:
-      buf += line
+    with open('$tmpfile', 'r') as f:
+        for i, line in enumerate(f):
+            if i % 32 == 0:
+                frame += 1
+                sys.stdout.write(buf)
+                buf = ''
+                elapsed = time.time() - begin
+                repose = (frame * time_per_frame) - elapsed
+                if repose > 0.0:
+                    time.sleep(repose)
+                next_frame = elapsed / time_per_frame
+            if frame >= next_frame:
+                buf += line
 except KeyboardInterrupt:
-  pass
+    pass
 EOF
-) < <(obtainium $video | bunzip2 -q 2> /dev/null)
+
+rm $tmpfile
